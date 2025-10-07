21dd0ed17591ef83f536ef44cd08366c1a5bbf56
Diego León <di.leonl@duocuc.cl>
2025-10-06 22:08:45 -0300

    Adición de rutas mision, vision, contacto

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Producto from './pages/Producto';
+import Mision from './pages/Mision';
+import Vision from './pages/Vision';
+import Contacto from './pages/Contacto';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="/producto/:id" element={<Producto/>} />
+          <Route path="/mision" element={<Mision/>} />
+          <Route path="/vision" element={<Vision/>} />
+          <Route path="/contacto" element={<Contacto/>} />
         </Routes>
       </main>
       <Footer/>
