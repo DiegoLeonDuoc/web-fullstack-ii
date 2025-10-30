241f52e78b8107a6f2976a4a8daa6a3a901a376a
Esteban Zenteno Espinoza <spowoky.uwu@gmail.com>
2025-10-30 00:29:19 -0300

    refactor: mejorar pruebas en ProductTable para mayor claridad y mantenimiento

--- src/tests-karma/product-table.spec.js
+++ src/tests-karma/product-table.spec.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 import ProductTable from "../components/ProductTable";
-
+const viniloImg = "/images/Vinilo.png";
+const cdImg = "/images/CD.png";
 // Revisamos que la tabla pinte todas las filas y dispare los botones de editar/eliminar.
 describe("ProductTable (Jasmine)", () => {
   afterEach(() => {
@@ -10,8 +11,8 @@ describe("ProductTable (Jasmine)", () => {
 
   it("renderiza todas las filas para los productos entregados", () => {
     const productos = [
-      { id: "p1", titulo: "Disco Uno", artista: "Artista A", formato: "CD", anio: 2020, etiqueta: "Label", precio: "$9.990", img: "/img/1.png" },
-      { id: "p2", titulo: "Disco Dos", artista: "Artista B", formato: "Vinilo", anio: 1999, etiqueta: "Label", precio: "$12.990", img: "/img/2.png" },
+      { id: "p1", titulo: "Disco Uno", artista: "Artista A", formato: "CD", anio: 2020, etiqueta: "Label", precio: "$9.990", img: cdImg },
+      { id: "p2", titulo: "Disco Dos", artista: "Artista B", formato: "Vinilo", anio: 1999, etiqueta: "Label", precio: "$12.990", img: viniloImg },
     ];
 
     render(
